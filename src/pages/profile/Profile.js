59476a713e437c395d84ac5e9a62d70ef6c7e904
Alice <xinranjiang2015@yahoo.com>
2022-12-21 19:36:10 -0500

    fixed bugs

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,68 +1,46 @@
-import React, { useState } from 'react';
-
-// ? initialize data
-// ? the first profile is incomplete
+import React, { useEffect, useState } from 'react';
 
 export default function Profile() {
-  const [user, setUser] = useState({});
-  const [name, setName] = useState('');
-  const [add, setAdd] = useState('');
-  const [pic, setPic] = useState('');
-  const [age, setAge] = useState('');
+  //need to initialize a dummy object like this:
+  const dummy = {
+    name: { first: '', last: '' },
+    gender: '',
+    picture: { large: '', medium: '', small: '' },
+    email: '',
+  };
 
-  async function getUser() {
-    setUser({});
-    setName('');
-    setAdd('');
-    setPic('');
-    setAge('');
+  const [user, setUser] = useState(dummy);
+
+  function getUser() {
+    //reset
+    setUser(dummy);
 
     fetch('https://randomuser.me/api/')
       .then((res) => res.json())
       .then((data) => {
         setUser(data.results[0]);
-        let name =
-          user.name.title + ' ' + user.name.first + ' ' + user.name.last;
-        setName(name);
-        let add =
-          user.location.street.number +
-          ' ' +
-          user.location.street.name +
-          ', ' +
-          user.location.city +
-          ', ' +
-          user.location.state +
-          ', ' +
-          user.location.country +
-          ', ' +
-          user.location.postcode;
-        setAdd(add);
-        setPic(user.picture.large);
-        setAge(user.dob.age);
       });
   }
 
+  //fetch data on page load
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
-    //? why can we not directly access object properties like this: {user.location.street.number}
     <div className="profile-container">
-      <img className="user-image" alt="user" src={pic}></img>
+      <img className="user-image" alt="user" src={user.picture.large}></img>
       <ul className="user-info">
         <li>
-          <span>Name: </span> {name}
+          <span>
+            Name: {user.name.first} {user.name.last}
+          </span>
         </li>
         <li>
           <span>Gender: </span> {user.gender}
         </li>
         <li>
           <span>Email: </span> {user.email}
-        </li>
-        <li>
-          <span>Location: </span>
-          {add}
-        </li>
-        <li>
-          <span>Age: </span>
-          {age}
         </li>
       </ul>
       <i className="fa-solid fa-right" onClick={getUser}></i>
